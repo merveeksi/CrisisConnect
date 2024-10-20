0310488f49ba1e2f2372311b461c5196f37ac0b6
MERVE EKŞİ <merveeksii61@gmail.com>
2024-10-21 00:23:48 +0300

    günceleme

--- src/CrisisConnect/CrisisConnect /Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ src/CrisisConnect/CrisisConnect /Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0431a6decb2f731a7a6f4846d92a0a24d10a4d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
